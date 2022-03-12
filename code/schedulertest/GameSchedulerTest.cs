33dde98249384a591be63f395604350360fb3d5d
SatishRanjan <satishranjan.github@gmail.com>
2022-03-11 20:05:18 -0800

    Schedule files

---
+++ code/schedulertest/GameSchedulerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ai_scheduler.src;
+using ai_scheduler.src.models;
+
+namespace AiSchedulerTest
+{
+    public class GameSchedulerTest
+    {
+        [Fact]
+        public void EntryPoint_Test()
+        {
+            GameScheduler schGen = new GameScheduler();
+            schGen.CreateMyCountrySchedule("Carpania", TestConstants.ResourceFileName, TestConstants.InitialWorldStateFileName, TestConstants.OutputScheduleFileName, 5, depthBound:7, 100);
+        }
+    }
+}
